f7dfdae308eae2386f0354ee8a74e765227f65d8
Peter <peterxu@Peters-MacBook-Pro.local>
2018-11-11 14:28:54 -0800

    `

--- background.js
+++ background.js
@@ -2,4 +2,8 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({color: '#3aa757'}, function() {
       console.log("UP RUNNING!");
     });
+    chrome.tabs.getSelected(null,function(tab) {
+        var tablink = tab.url;
+        console.log(tablink);
+    });
   });
