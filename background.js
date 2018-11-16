af6ad5d7bcb92b6d440b5f28136aa5c75e88051d
Gustavo Vieira <oncodgamer@gmail.com>
2018-11-16 15:51:56 -0800

    Prettier

--- background.js
+++ background.js
@@ -1,16 +1,15 @@
 chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.sync.set({color: '#3aa757'}, function() {
-      console.log("UP RUNNING!");
-    });
+  chrome.storage.sync.set({ color: "#3aa757" }, function() {
+    console.log("UP RUNNING!");
   });
-  chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-    if (changeInfo.status == 'complete') {
-        var tablink = tab.url;
-        console.log(tablink);
-        chrome.storage.local.set({'value': tablink}, function() {
-            // Notify that we saved.
-            console.log('URL saved');
-          });
-    }
-  })
-  
+});
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if (changeInfo.status == "complete") {
+    var tablink = tab.url;
+    console.log(tablink);
+    chrome.storage.local.set({ value: tablink }, function() {
+      // Notify that we saved.
+      console.log("URL saved");
+    });
+  }
+});
--- get-url.js
+++ get-url.js
@@ -1,4 +1,7 @@
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
+chrome.tabs.query(
+  { active: true, lastFocusedWindow: true, currentWindow: true },
+  function(tabs) {
     var url = tabs[0].url;
     console.log(url);
-});
+  }
+);
