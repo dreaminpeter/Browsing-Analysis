e8aeaf99d7803d909f5dd4b47418d3215906b9cd
Peter <peterxu@Peters-MacBook-Pro.local>
2018-11-18 15:06:19 -0800

    sync

--- background.js
+++ background.js
@@ -1,8 +1,17 @@
+//check initialization
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ color: "#3aa757" }, function() {
-    console.log("UP RUNNING!");
+  chrome.storage.sync.get(['visits'], function(result) {
+    console.log('Value currently is ' + result.visits);
+    if (result.visits == undefined) {
+      var currentDate = Date.now();
+      chrome.storage.sync.set({'visits': "*extension initialized!*" ,currentDate});
+      console.log("Initialized");
+    }else{
+      console.log("Ready");
+    }
   });
 });
+//URL listener
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status == "complete") {
     var tablink = tab.url;
