086e448f53627f364385e7ce68c430b21544ca95
Gustavo Vieira <oncodgamer@gmail.com>
2018-12-04 16:25:10 -0800

    Save host to chrome's storage.

--- background.js
+++ background.js
@@ -1,18 +1,19 @@
 //check initialization
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.get(['inintialization'], function(result) {
-    console.log('Value currently is ' + result.inintialization);
+  chrome.storage.sync.get(["inintialization"], function(result) {
+    console.log("Value currently is " + result.inintialization);
     if (result.inintialization == undefined) {
-      var visits = [  Date.now() + ',' + 'inintialization'];
-      chrome.storage.sync.set({'visits': visits}, function(){
-        console.log('Empty database(visits) created!');
-
+      var visits = [Date.now() + "," + "inintialization"];
+      chrome.storage.sync.set({ visits: visits }, function() {
+        console.log("Empty database(visits) created!");
       });
       var currentDate = Date.now();
-      chrome.storage.sync.set({'inintialization': '*extension initialized!*' + currentDate});
+      chrome.storage.sync.set({
+        inintialization: "*extension initialized!*" + currentDate
+      });
       console.log(result.inintialization);
-    }else{
-      console.log('Ready');
+    } else {
+      console.log("Ready");
     }
   });
 });
@@ -20,21 +21,25 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status == "complete") {
     const tablink = tab.url;
+
     if (tablink == undefined) {
-      return
+      return;
     }
+
+    const url = new URL(tablink);
+    console.log(url);
+
     var currentDate = Date.now();
     console.log(tablink);
-    chrome.storage.sync.get(['visits'], function(result){
+    chrome.storage.sync.get(["visits"], function(result) {
       var visits = result.visits;
-      visits.push(currentDate + ',' + tablink);
-      chrome.storage.sync.set({'visits': visits}, function() {
+      visits.push(currentDate + "," + url.host);
+      chrome.storage.sync.set({ visits: visits }, function() {
         // Notify that we saved.
         console.log("URL saved");
+      });
     });
-    
-    });
-  }else{
-    return
+  } else {
+    return;
   }
 });
