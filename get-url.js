fc4c90fefe519a7c6724127dc5bcbadc4ca4ff61
Nando Vieira <me@fnando.com>
2019-01-20 12:22:23 -0800

    Add sample pie chart. Also organize code.

--- get-url.js
+++
@@ -1,7 +0,0 @@
-chrome.tabs.query(
-  { active: true, lastFocusedWindow: true, currentWindow: true },
-  function(tabs) {
-    var url = tabs[0].url;
-    console.log(url);
-  }
-);
--- js/dashboard.js
+++ js/dashboard.js
@@ -1,4 +1,43 @@
-checkstatus();
+function drawChart() {
+  const ctx = document.getElementById("piechart").getContext("2d");
+
+  const chart = new Chart(ctx, {
+      type: 'doughnut',
+      data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+}
 
 chrome.storage.local.get(["visits"], function (result) {
     const visits = result.visits;
@@ -10,3 +49,7 @@ chrome.storage.local.get(["visits"], function (result) {
 
     console.log('-------->', totalTimeInMinutes);
 });
+
+checkstatus();
+setInterval(checkstatus, 5000);
+drawChart();
